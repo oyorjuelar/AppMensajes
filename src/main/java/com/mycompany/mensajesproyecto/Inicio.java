51e954ff64e668176f001328980fe6f1e63f8002
Omar Orjuela <omarorjuela95@gmail.com>
2020-01-21 00:45:19 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/mensajesproyecto/Inicio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajesproyecto;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Tatiana Orjuela R
+ */
+public class Inicio {
+    
+    public static void main(String[] args){
+        Conexion conexion = new Conexion();
+        try(Connection cnx = conexion.get_connection()){
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+}
