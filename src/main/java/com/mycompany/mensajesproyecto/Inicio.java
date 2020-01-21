f4bc765064912bdea15da844e49a07635f1cf14e
Omar Orjuela <omarorjuela95@gmail.com>
2020-01-21 01:59:46 -0500

    Capas DAO, Service e Inicio creadas

--- src/main/java/com/mycompany/mensajesproyecto/Inicio.java
+++ src/main/java/com/mycompany/mensajesproyecto/Inicio.java
@@ -6,6 +6,7 @@
 package com.mycompany.mensajesproyecto;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -14,12 +15,42 @@ import java.sql.Connection;
 public class Inicio {
     
     public static void main(String[] args){
-        Conexion conexion = new Conexion();
-        try(Connection cnx = conexion.get_connection()){
+        
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        
+        do{
+            System.out.println("--------");
+            System.out.println("Aplicación de mensajes");
+            System.out.println("1. Crear mensaje");
+            System.out.println("2. Listar mensaje");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Eliminar mensaje");
+            System.out.println("5. Salir");
             
-        }catch(Exception e){
-            System.out.println(e);
-        }
+            opcion = sc.nextInt();
+            
+            switch (opcion){
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.editarMensaje();
+                    break;
+                case 4:
+                    MensajesService.borrarMensaje();
+                    break;
+                default:
+                    break;
+                
+            }
+            
+            
+        }while(opcion != 5);
+        
         
     }
     
